a8ea6f3f6295e23cea11d829c718b919d1e185fb
Princeton J. Rose <princetonjrose@gmail.com>
2022-12-29 01:17:48 -0500

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
+def roll_call_dwarves( dwarves ):
     pass
+    [ print( f"{ index + 1 }. { dwarf }" ) for index, dwarf in enumerate( dwarves ) ]
 
-def summon_captain_planet():
+def summon_captain_planet( elements ):
     pass
+    return [ f'{ element.title() }!' for element in elements ]
 
-def long_planeteer_calls():
+def long_planeteer_calls( words ):
     pass
+    for word in words:
+        if len( word ) > 4:
+            return True
+    else: return False
 
-def find_the_cheese():
+def find_the_cheese( ingredients ):
     pass
+    chesses = [ 'cheddar', 'gouda', 'camembert' ]
+    for i in ingredients:
+        if i in chesses:
+            return i
+    else: return None
